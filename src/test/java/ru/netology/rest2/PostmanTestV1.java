6e33af564f383382c8ebeea367535af1479a75a9
Aleksandra Zhukova <a.hahina@gmail.com>
2021-02-08 22:50:37 +0300

    postman with bad test

---
+++ src/test/java/ru/netology/rest2/PostmanTestV1.java
@@ -0,0 +1,24 @@
+package ru.netology.rest2;
+
+import io.restassured.http.ContentType;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+public class PostmanTestV1 {
+    @Test
+    public void shouldReturnDemoAccounts() {
+        given()
+            .baseUri("https://postman-echo.com")
+            .contentType("text/plain; charset=UTF-8")
+            .body("some data")
+       .when()
+            .post("/post")
+       .then()
+            .statusCode(200)
+            .body("data", equalTo("some qwqw"))
+        ;
+    }
+}
